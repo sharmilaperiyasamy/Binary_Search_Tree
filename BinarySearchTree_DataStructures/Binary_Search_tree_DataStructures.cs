c3a72569728a6854620c3cc8b5d03a943c0daad9
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-10 18:40:46 +0530

    [Sharmila] create the binary search tree

---
+++ BinarySearchTree_DataStructures/Binary_Search_tree_DataStructures.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_DataStructures
+{
+    internal class Binary_Search_tree_DataStructures<T> where T : IComparable<T>
+    {
+            public T data;
+            public Binary_Search_tree_DataStructures<T> left;
+            public Binary_Search_tree_DataStructures<T> right;
+            public Binary_Search_tree_DataStructures(T data)
+            {
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
+            int leftIdx = 0, rightIdx = 0;
+            public void AddNode(T value)
+            {
+                T nodeValue = this.data;
+                if (nodeValue.CompareTo(value) > 0)
+                {
+                    if (this.left == null)
+                        this.left = new Binary_Search_tree_DataStructures<T>(value);
+                    else
+                        this.left.AddNode(value);
+                }
+                else
+                {
+                    if (this.right == null)
+                        this.right = new Binary_Search_tree_DataStructures<T>(value);
+                    else
+                        this.right.AddNode(value);
+                }
+            }
+            public void Display()
+            {
+                if (this.left != null)
+                {
+                    this.leftIdx++;
+                    this.left.Display();
+                }
+                Console.WriteLine(this.data);
+                if (this.right != null)
+                {
+                    rightIdx++;
+                    this.right.Display();
+                }
+            }
+    }
+}
+
---
+++ BinarySearchTree_DataStructures/Program.cs
@@ -0,0 +1,4 @@
+﻿BinarySearchTree_DataStructures.Binary_Search_tree_DataStructures<int> tree = new BinarySearchTree_DataStructures.Binary_Search_tree_DataStructures<int>(56);
+tree.AddNode(30);
+tree.AddNode(70);
+tree.Display();
